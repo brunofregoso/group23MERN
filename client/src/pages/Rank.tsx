46cdc31f7452f9548a575af50a056ce9ca0d9ac1
pleber2021 <lebercontact@gmail.com>
2024-11-05 17:40:11 -0500

    New Skeleton with Folder of Old
    Hopefully this will provide a skeleton we can use to start building the website. A folder with the old untouched code it there if this doesn't work out.

---
+++ client/src/pages/Rank.tsx
@@ -0,0 +1,11 @@
+import PageWrapper from "src/components/PageWrapper";
+
+const Rank = () => (
+  <PageWrapper title="Rank">
+    <p className="tc f4 fw4 w-70">
+      Leaderboard
+    </p>
+  </PageWrapper>
+);
+
+export default Rank;
